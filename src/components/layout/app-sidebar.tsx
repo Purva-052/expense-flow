466423734b4bd8961c164a2832eb29a0afdb0845
Yug Joshi <yug.j@devstree.in>
2025-10-14 18:46:12 +0530

    ✨ add padding in sidebar logo

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -34,10 +34,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
   return (
     <Sidebar collapsible="icon" variant="sidebar" {...props}>
-      <SidebarHeader className="cursor-pointer  !shadow-sm ">
+      <SidebarHeader className="cursor-pointer p-2  !shadow-sm ">
         <Link to="/">
           <img
-            className="h-20 w-full group-data-[state=collapsed]:h-10"
+            className="h-12 w-full group-data-[state=collapsed]:h-10"
             src={sidebarLogo}
             alt="klub Logo"
           />
