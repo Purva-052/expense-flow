72c8c3080dd462be0565694633b0c74e53bade51
Yug Joshi <yug.j@devstree.in>
2025-10-29 17:48:16 +0530

    ✨ developer role can not access resource tab

--- src/features/kanban-board/index.tsx
+++ src/features/kanban-board/index.tsx
@@ -2,27 +2,35 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import Board from "./components/Board";
 import { Main } from "@/components/layout/main";
 import ResourceTab from "./components/resourceTab";
+import { useAuthStore } from "@/stores/use-auth-store";
 
 const ProjectBoard = () => {
+  const { user } = useAuthStore();
+  const userRole = user?.user?.role;
   return (
     <Main>
-      <Tabs defaultValue="board" className="w-full">
-        {/* Tab Headers */}
-        <TabsList className="grid grid-cols-2 w-[300px] mb-2">
-          <TabsTrigger value="board">Projects</TabsTrigger>
-          <TabsTrigger value="resources">Resources</TabsTrigger>
-        </TabsList>
+      {userRole === "developer" ? (
+        <Board />
+      ) : (
+        // 🧭 Others see tabbed layout
+        <Tabs defaultValue="board" className="w-full">
+          {/* Tab Headers */}
+          <TabsList className="grid grid-cols-2 w-[300px] mb-2">
+            <TabsTrigger value="board">Projects</TabsTrigger>
+            <TabsTrigger value="resources">Resources</TabsTrigger>
+          </TabsList>
 
-        {/* Board Tab */}
-        <TabsContent value="board">
-          <Board />
-        </TabsContent>
+          {/* Board Tab */}
+          <TabsContent value="board">
+            <Board />
+          </TabsContent>
 
-        {/* Resources Tab */}
-        <TabsContent value="resources">
-          <ResourceTab />
-        </TabsContent>
-      </Tabs>
+          {/* Resources Tab */}
+          <TabsContent value="resources">
+            <ResourceTab />
+          </TabsContent>
+        </Tabs>
+      )}
     </Main>
   );
 };
