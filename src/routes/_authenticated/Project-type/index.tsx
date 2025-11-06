47e8959ca42b016710a03330f47206b176e4848d
Yug Joshi <yug.j@devstree.in>
2025-11-06 12:20:40 +0530

    ✨ add validation for protected routes

--- src/routes/_authenticated/Project-type/index.tsx
+++ src/routes/_authenticated/Project-type/index.tsx
@@ -1,8 +1,10 @@
 import ProjectTypePage from "@/features/Project-type";
+import { requireRole } from "@/utils/requireRole";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/Project-type/")({
   component: RouteComponent,
+   beforeLoad: () => requireRole(["admin", "team_lead", "project_manager"]),
 });
 
 function RouteComponent() {
--- src/routes/_authenticated/users/index.tsx
+++ src/routes/_authenticated/users/index.tsx
@@ -1,8 +1,10 @@
 import UsersPage from "@/features/users";
+import { requireRole } from "@/utils/requireRole";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/users/")({
   component: RouteComponent,
+   beforeLoad: () => requireRole(["admin", "team_lead", "project_manager"]),
 });
 
 function RouteComponent() {
